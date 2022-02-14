e2abe2f37618b96bb87d05b126efc4b15595da17
Paul Shertzinger <pshertzi@gmail.com>
2022-02-14 18:35:51 -0500

    html generated

--- project/app.js
+++ project/app.js
@@ -1,4 +1,6 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const generatePage = require('./page-template');
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -29,10 +31,16 @@ const promptUser = () => {
       }
     },
     {
+      type: 'confirm',
+      name: 'confirmAbout',
+      message: 'Would you like to enter some information about yourself for an "About" section?',
+      default: true
+    },
+    {
       type: 'input',
       name: 'about',
-      message: 'Provide some information about yourself:'
-      
+      message: 'Provide some information about yourself:',
+      when: ({ confirmAbout }) => confirmAbout
     }
   ]);
 };
@@ -119,18 +127,20 @@ if (!portfolioData.projects) {
   promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html',pageHTML, err => {
+    if (err) throw new Error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+    });
   });
-//const fs = require('fs');
-
-//const generatePage = require('./page-template');
-
-//const pageHTML = generatePage(name, github);
 
 
 
-//fs.writeFile('./index.html', generatePage(name, github), err => {
-  //if (err) throw new Error(err);
 
-  //console.log('Portfolio complete! Check out index.html to see the output!');
-//});
+
+
+
+
+  
