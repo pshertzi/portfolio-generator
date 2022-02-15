5f02f26d4c9354a3738929ce767b01d9747e2003
Paul Shertzinger <pshertzi@gmail.com>
2022-02-15 10:50:19 -0500

    code refracted

--- project/utils/generate-site.js
+++ project/utils/generate-site.js
@@ -1,7 +1,35 @@
 const fs = require('fs');
-const { reject } = require('lodash');
-const { resolve } = require('path/posix');
 
-const writeFile = filecontent => {
-    return new Promise((resolve, reject) =>{});
+const writeFile = fileContent => {
+    return new Promise((resolve, reject) =>{
+        fs.writeFile('./dist/index.html', fileContent, err => {
+            //if there is an error reject the Promise and send the error to the Promise's `.catch()` method
+            if (err) {
+                reject(err);
+                // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
+                return;
+            }
+            // if everything went well, resolve the Promise and send the successful data to the `.then()` method
+            resolve({
+                ok: true,
+                message: 'file created!'
+            });
+        });
+    });
 };
+const copyFile = () => {
+    return new Promise ((resolve, reject) => {
+        fs.copyFile('./src/style.css', './dist/style.css', err => {
+            if (err) {
+                reject (err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: 'stylesheet created!'
+            });
+        });
+    });
+};
+
+module.exports = {writeFile,copyFile};
