614bc0aaccd6130b8f1410fe89aee0d55a9f3a4b
Paul Shertzinger <pshertzi@gmail.com>
2022-02-14 21:25:27 -0500

    new items added

--- project/app.js
+++ project/app.js
@@ -127,16 +127,21 @@ if (!portfolioData.projects) {
   promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-
-    fs.writeFile('./index.html',pageHTML, err => {
-    if (err) throw new Error(err);
-
-    console.log('Portfolio complete! Check out index.html to see the output!');
-    });
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
-
-
 
 
 
---
+++ project/utils/generate-site.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const { reject } = require('lodash');
+const { resolve } = require('path/posix');
+
+const writeFile = filecontent => {
+    return new Promise((resolve, reject) =>{});
+};
